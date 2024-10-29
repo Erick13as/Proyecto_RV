af81d5a2190b2fb65f51d23c7434df5d9685ab0c
Erick13as <alvaradosolanoerick@gmail.com>
2024-10-29 17:13:19 -0600

    Update CambiarModelo.cs

--- Assets/CambiarModelo.cs
+++ Assets/CambiarModelo.cs
@@ -4,39 +4,43 @@ using UnityEngine;
 
 public class CambiarModelo : MonoBehaviour
 {
-    // Referencias a los modelos 
+    // Referencias a los modelos
     public GameObject modeloInicial;
     public GameObject modeloConMaterial;
     
+    // Tags configurables desde el Inspector
+    public string tagParaRecoger = "Tortilla";
+    public string tagParaSoltar = "Plato";
+    
     private bool tienePollo = false;
-
+    
     private void Start()
     {
         if (modeloInicial != null) modeloInicial.SetActive(true);
         if (modeloConMaterial != null) modeloConMaterial.SetActive(false);
     }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         // Para recoger el material
-        if (other.CompareTag("Tortilla") && !tienePollo)
+        if (other.CompareTag(tagParaRecoger) && !tienePollo)
         {
             CambiarAModeloConMaterial();
         }
         // Para soltar el material
-        else if (other.CompareTag("Plato") && tienePollo)
+        else if (other.CompareTag(tagParaSoltar) && tienePollo)
         {
             CambiarAModeloInicial();
         }
     }
-
+    
     private void CambiarAModeloConMaterial()
     {
         modeloInicial.SetActive(false);
         modeloConMaterial.SetActive(true);
         tienePollo = true;
     }
-
+    
     private void CambiarAModeloInicial()
     {
         modeloInicial.SetActive(true);
